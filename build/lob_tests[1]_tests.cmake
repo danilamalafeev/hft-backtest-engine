86c8f74032cfbd79897d44cac64765e4b3f015d3
Danila Malafeev <danilamalafeev@Danilas-Laptop.local>
2026-04-24 14:47:43 +0300

    base

--- build/lob_tests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[OrderBookTest.InitializesSuccessfully]=]  [==[/Users/danilamalafeev/Documents/New project/build/lob_tests]==] [==[--gtest_filter=OrderBookTest.InitializesSuccessfully]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[OrderBookTest.InitializesSuccessfully]=]  PROPERTIES DEF_SOURCE_LINE [==[/Users/danilamalafeev/Documents/New project/tests/test_lob.cpp:7]==] WORKING_DIRECTORY [==[/Users/danilamalafeev/Documents/New project/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  lob_tests_TESTS OrderBookTest.InitializesSuccessfully)
